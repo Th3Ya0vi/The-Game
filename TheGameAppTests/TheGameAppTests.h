54efcd324781a17e0672574bcdbddec86ff68450
denisdbv <denisdbv@gmail.com>
2013-07-21 10:53:18 +0400

    Initial Commit

---
+++ TheGameAppTests/TheGameAppTests.h
@@ -0,0 +1,13 @@
+//
+//  TheGameAppTests.h
+//  TheGameAppTests
+//
+//  Created by DenisDbv on 21.07.13.
+//  Copyright (c) 2013 axbx. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TheGameAppTests : SenTestCase
+
+@end
---
+++ TheGameAppTests/TheGameAppTests.m
@@ -0,0 +1,32 @@
+//
+//  TheGameAppTests.m
+//  TheGameAppTests
+//
+//  Created by DenisDbv on 21.07.13.
+//  Copyright (c) 2013 axbx. All rights reserved.
+//
+
+#import "TheGameAppTests.h"
+
+@implementation TheGameAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TheGameAppTests");
+}
+
+@end
